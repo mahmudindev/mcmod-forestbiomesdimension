e42b08eb11aefa7a0bca5cb3a70796571b3693ab
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2025-08-09 05:02:13 +0700

    Updates to 1.21

---
+++ neoforge/src/main/java/com/github/mahmudindev/mcmod/forestbiomesdimension/neoforge/ForestBiomesDimensionNeoForge.java
@@ -0,0 +1,12 @@
+package com.github.mahmudindev.mcmod.forestbiomesdimension.neoforge;
+
+import com.github.mahmudindev.mcmod.forestbiomesdimension.ForestBiomesDimension;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ForestBiomesDimension.MOD_ID)
+public final class ForestBiomesDimensionNeoForge {
+    public ForestBiomesDimensionNeoForge() {
+        // Run our common setup.
+        ForestBiomesDimension.init();
+    }
+}
